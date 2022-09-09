36463338b1e2416cf9a96c70a0cb2cb29ab7ccf0
Martin Schenck <martin.schenck@futurice.com>
2022-09-09 14:01:21 +0200

    Move paddle within bounds when growing

--- Entities/Paddle/Paddle.gd
+++ Entities/Paddle/Paddle.gd
@@ -10,12 +10,15 @@ export var max_height := 400
 var direction := Vector2()
 var height: float
 
+var top_border := 88
+var viewport_height := 600
+
 
 func _ready():
 	call_deferred("create_unique_shape")
 
 
-func create_unique_shape():
+func create_unique_shape() -> void:
 	# Create each their own shape so that they can grow independently
 	var shape := RectangleShape2D.new()
 	shape.extents = $CollisionShape2D.get_shape().extents
@@ -35,7 +38,7 @@ func _physics_process(delta):
 		var _collision = move_and_collide(direction * delta)
 
 
-func increase_height(amount: int):
+func increase_height(amount: int) -> void:
 	if $ColorRect.rect_size.y >= max_height:
 		return
 
@@ -44,3 +47,18 @@ func increase_height(amount: int):
 
 	var collision_shape = $CollisionShape2D.get_shape()
 	collision_shape.extents.y += amount
+
+	# Move paddle inside field if grown outside.
+	move_paddle_within_bounds()
+
+
+func move_paddle_within_bounds() -> void:
+	var extents: Vector2 = $CollisionShape2D.get_shape().extents
+
+	var distance_from_top: float = position.y - extents.y - top_border
+	if distance_from_top < 0:
+		position.y -= distance_from_top
+
+	var distance_from_bottom: float = viewport_height - (position.y + extents.y)
+	if distance_from_bottom < 0:
+		position.y += distance_from_bottom
